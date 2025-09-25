c160c703af9a9d3d846d2ccb621c9b7a3b5bfab3
Adham Yuldashev <adhamyuldashev72@gmail.com>
2025-09-25 22:17:11 +0500

    enter number until player wins

--- guess.py
+++ guess.py
@@ -1,9 +1,9 @@
 import random
 
 n =  random.randint(0, 10)
-guess = int(input("guess a number between 0 and 10: "))
 
 while True:
+    guess = int(input("guess a number between -3 and 10: "))
     if guess == n:
         print("Topdingiz")
         break
