dd8388a61c2310dd5dfcbb33da8318f56c5519bf
Adham Yuldashev <adhamyuldashev72@gmail.com>
2025-09-25 22:01:04 +0500

    repeat until player wins

--- guess.py
+++ guess.py
@@ -3,7 +3,7 @@ import random
 n =  random.randint(0, 10)
 guess = int(input("guess a number between 0 and 10: "))
 
-if guess == n:
-    print("Topdingiz")
-else:
-    print("Topa olmadingiz")
+while True:
+    if guess == n:
+        print("Topdingiz")
+        break
